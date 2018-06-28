66bd79dcf56a455f13b73b115dc2fbff4584ba62
Shuaib Sajid <shusajid@gmail.com>
2018-06-28 09:10:15 -0500

    lecture starting, will get back too.

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 //get
 $.ajax("http://localhost:3000/employee").then(allEmployee => console.log(allEmployee))
 $.ajax("http://localhost:3000/employeeStatus").then(allEmployeeDatabase => console.log(allEmployeeDatabase))
+
+const divRef = document.querySelector(".employee-card");
+const $artEmployee = ("<article>").addClass("employee").appendTo(divRef)
+const $h1Name = ("<h1>").addClass
